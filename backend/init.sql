74d1986b142722f7c958c8d745f06f16af5585bc
Pauline-Mac <pauline.maceiras@gmail.com>
2025-02-20 18:00:19 +0100

    Set up db prostgre

---
+++ backend/init.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS users (
+                                     id SERIAL PRIMARY KEY,
+                                     name TEXT NOT NULL,
+                                     email TEXT UNIQUE NOT NULL
+);
+
+INSERT INTO users (name, email) VALUES
+                                    ('John Doe', 'johndoe@example.com'),
+                                    ('Jane Smith', 'janesmith@example.com'),
+                                    ('Alice Johnson', 'alice.johnson@example.com');
