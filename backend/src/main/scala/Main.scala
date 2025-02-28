c7a8161327ea45fd96aea43a79469c76109f9553
Pauline-Mac <pauline.maceiras@gmail.com>
2025-02-28 01:26:21 +0100

    reorganisation plus ajout AVClient fonctionnalitées

--- backend/src/main/scala/Main.scala
+++ backend/src/main/scala/Main.scala
@@ -1,89 +1,95 @@
 import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
-import akka.actor.typed.ActorRef
+import akka.actor.ActorSystem => ClassicActorSystem
 import org.mindrot.jbcrypt.BCrypt
 import actors.UserActor
-import scala.concurrent.Await
-import scala.concurrent.duration.Duration
 import controllers.API.AlphaVantageClient
 
-
 object Main extends App {
-
+  // Function to print the CyStonks message
   def printCyStonksMessage(): Unit = {
-    println("Welcome to ")
-    println("""
-  ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              |⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              |⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              |⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              |⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              |⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⠤⠐⠒⠀⠀⠀⠒⠒⠤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              |⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡠⠊⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⢄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              |⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡔⠁⠀⠀⠀⠀⠀⢰⠁⠀⠀⠀⠀⠀⠀⠈⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              |⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠀⠀⠀⠀⠀⠀⠀⣾⠀⠀⠔⠒⠢⠀⠀⠀⢼⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              |⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡆⠀⠀⠀⠀⠀⠀⠀⠸⣆⠀⠀⠙⠀⠀⠠⠐⠚⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              |⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠇⠀⠀⠀⠀⠀⠀⠀⠀⢻⠀⠀⠀⠀⠀⠀⡄⢠⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣤⣶⣽⡆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              |⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⣀⣀⡠⡌⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣴⣾⣯⣿⣿⣿⡏⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              |⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠆⠀⠀⠀⠀⠀⠀⠀⠘⡆⠀⠀⢀⣀⡀⢠⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⢴⣾⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              |⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠠⢄⠀⠀⢣⠀⠀⠑⠒⠂⡌⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              |⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⠤⡀⠑⠀⠀⠀⡘⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡀⣡⣿⣿⣿⣿⣿⣿⣿⣟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              |⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡄⠀⠀⠀⠀⠀⠀⠀⠈⢑⠖⠒⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠐⣴⣿⣿⣿⡟⠁⠈⠛⠿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              |⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣄⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⢈⣾⣿⣿⣿⠏⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              |⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣷⡤⣀⡀⠀⠀⢀⠎⣦⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣢⣿⣿⣿⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              |⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣿⣄⠈⢒⣤⡎⠀⢸⣿⣿⣿⣷⣶⣤⣄⣀⠀⠀⠀⢠⣽⣿⠿⠿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              |⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣾⠛⠉⣿⣦⣸⣿⣿⣿⣿⣿⣿⣿⣿⣿⡗⣰⣿⣿⣿⠀⠀⣿⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              |⠀⠀⠀⠀⣀⡀⠀⢀⠀⠀⢠⠄⡰⠋⠉⠉⠉⣿⠉⠀⠀⠉⢹⡿⠋⠉⠉⠉⠛⢿⣿⠉⠉⠋⠉⠉⠻⣿⠀⠀⣿⠞⠉⢉⣿⠚⠉⠉⠉⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              |⠀⠀⠀⡜⠀⠀⠀⠈⢣⡰⠋⠀⢧⠀⠈⠛⠿⣟⢻⠀⠀⣿⣿⠁⠀⣾⣿⣧⠀⠘⣿⠀⠀⣾⣿⠀⠀⣿⠀⠀⠋⠀⢰⣿⣿⡀⠀⠛⠻⣟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              |⠀⠀⢰⠁⠀⠀⠀⠀⠈⡇⠀⠀⡞⠿⠶⠄⠀⢺⢼⠀⠀⠿⢿⡄⠀⠻⠿⠇⠀⣸⣿⠀⠀⣿⣿⠀⠀⣿⠀⠀⣶⡀⠈⢻⣿⠿⠶⠆⠀⢸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              |⠀⠀⢸⠀⠀⢀⡀⠀⠀⢦⠀⠀⠧⢤⣤⣤⠴⠋⠈⠦⣤⣤⠼⠙⠦⢤⣤⡤⠶⠋⠹⠤⠤⠿⠿⠤⠤⠿⠤⠤⠿⠳⠤⠤⠽⢤⣤⣤⠴⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              |⠀⠀⠈⠓⠒⠉⠀⠀⠀⠘⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              |⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              |⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              |⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
-              |⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀""".stripMargin)
+    println("Welcome to CyStonks!")
+    println(""" ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ """)
   }
 
+  // Create the typed actor system for portfolio management
+  implicit val system: ActorSystem[Nothing] = ActorSystem(Behaviors.empty, "PortfolioSystem")
 
+  // Create the AlphaVantage client actor
+  val alphaVantageClient = system.systemActorOf(AlphaVantageClient(), "alphaVantageClient")
 
-  val system = ActorSystem(Behaviors.setup[UserActor.Portfolio] { context =>
-    printCyStonksMessage()
-    // Create a user actor
-    val userActor = context.spawn(UserActor(), "user-1")
-
-    // Add assets to the user portfolio
-    userActor ! UserActor.AddAsset("AAPL", 10)
-    userActor ! UserActor.AddAsset("BTC", 2)
-
-    // Fetch and print the user's portfolio
-    userActor ! UserActor.GetPortfolio(context.self)
-
-    // Insert the user into the database with a hashed password using BCrypt
-    val password = "securePassword123"
-    val hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt()) // Hash the password
-
-    val userCreatedActor = context.spawn(Behaviors.receiveMessage[Boolean] {
-      case true =>
-        println("User successfully created in the database!")
+  // Create a response handler actor
+  val responseHandler = system.systemActorOf(
+    Behaviors.receiveMessage[AlphaVantageClient.Response] {
+      case AlphaVantageClient.StockPriceResponse(symbol, Some(price)) =>
+        println(s"Current price of $symbol: $price")
         Behaviors.same
-      case false =>
-        println("Failed to create user in the database.")
+      case AlphaVantageClient.StockPriceResponse(symbol, None) =>
+        println(s"No price data available for $symbol")
         Behaviors.same
-    }, "userCreatedActor")
+      case AlphaVantageClient.TopGainersResponse(gainers) =>
+        println("Top Gainers:")
+        gainers.foreach(ticker => println(s"- $ticker"))
+        Behaviors.same
+      case AlphaVantageClient.RSIResponse(symbol, Some(rsi)) =>
+        println(s"Current RSI of $symbol: $rsi")
+        Behaviors.same
+      case AlphaVantageClient.RSIResponse(symbol, None) =>
+        println(s"No RSI data available for $symbol")
+        Behaviors.same
+      case AlphaVantageClient.ErrorResponse(message) =>
+        println(s"Error: $message")
+        Behaviors.same
+    },
+    "responseHandler"
+  )
 
-    // Create a new user and insert into the database with the hashed password
-    userActor ! UserActor.CreateUser("Pauline Maceiras", "pauline.maceiras@example.com", hashedPassword, userCreatedActor)
+  // Make multiple API calls
+  alphaVantageClient ! AlphaVantageClient.GetStockPrice("AAPL", responseHandler)
+  alphaVantageClient ! AlphaVantageClient.GetTopGainers(responseHandler)
+  alphaVantageClient ! AlphaVantageClient.GetRSI("MSFT", responseHandler)
 
-    // Receive the portfolio and print it out
-    Behaviors.receiveMessage {
-      case portfolio @ UserActor.Portfolio(assets) =>
-        println(s"User portfolio: $assets")
-        Behaviors.stopped
-    }
-  }, "PortfolioSystem")
+  // Functionality for portfolio management and user management
+  system.systemActorOf(
+    Behaviors.setup[UserActor.Portfolio] { context =>
+      printCyStonksMessage()
 
-  val stockDataActor = system.systemActorOf(AlphaVantageClient.stockDataActor, "stockDataActor")
-  stockDataActor ! "fetchStockData"
+      // Create a user actor
+      val userActor = context.spawn(UserActor(), "user-1")
+
+      // Add assets to the user portfolio
+      userActor ! UserActor.AddAsset("AAPL", 10)
+      userActor ! UserActor.AddAsset("BTC", 2)
+
+      // Fetch and print the user's portfolio
+      userActor ! UserActor.GetPortfolio(context.self)
+
+      // Insert the user into the database with a hashed password using BCrypt
+      val password = "securePassword123"
+      val hashedPassword = BCrypt.hashpw(password, BCrypt.gensalt()) // Hash the password
+
+      val userCreatedActor = context.spawn(Behaviors.receiveMessage[Boolean] {
+        case true =>
+          println("User successfully created in the database!")
+          Behaviors.same
+        case false =>
+          println("Failed to create user in the database.")
+          Behaviors.same
+      }, "userCreatedActor")
+
+      // Create a new user and insert into the database with the hashed password
+      userActor ! UserActor.CreateUser("Pauline Maceiras", "pauline.maceiras@example.com", hashedPassword, userCreatedActor)
+
+      // Receive the portfolio and print it out
+      Behaviors.receiveMessage {
+        case portfolio @ UserActor.Portfolio(assets) =>
+          println(s"User portfolio: $assets")
+          Behaviors.same
+      }
+    },
+    "portfolioReceiver"
+  )
+
+  // Wait to receive the stock data (add sleep to give the request time to complete)
   Thread.sleep(5000)
-
 }
--- backend/src/main/scala/controllers/API/AlphaVantageClient.scala
+++ backend/src/main/scala/controllers/API/AlphaVantageClient.scala
@@ -1,55 +1,114 @@
+// In file controllers/API/AlphaVantageClient.scala
 package controllers.API
 
-import akka.actor.typed.{ActorRef, Behavior}
-import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.unmarshalling.Unmarshal
+import akka.actor.typed.{ActorRef, Behavior}
+import akka.actor.typed.scaladsl.Behaviors
 import org.json4s._
 import org.json4s.native.JsonMethods._
-import scala.concurrent.{ExecutionContextExecutor, Future}
+import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
 
 object AlphaVantageClient {
-  implicit val system: akka.actor.typed.ActorSystem[Nothing] = akka.actor.typed.ActorSystem(Behaviors.empty, "AlphaVantageClient")
-  implicit val executionContext: ExecutionContextExecutor = system.executionContext
+  // Define the message protocol for the actor
+  sealed trait Command
 
-  // Importing DefaultFormats to allow easy extraction of values from JSON
-  implicit val formats: Formats = DefaultFormats
+  // Commands (requests)
+  case class GetStockPrice(symbol: String, replyTo: ActorRef[Response]) extends Command
+  case class GetTopGainers(replyTo: ActorRef[Response]) extends Command
+  case class GetRSI(symbol: String, replyTo: ActorRef[Response]) extends Command
 
-  val apiKey: String = sys.env.getOrElse("ALPHA_VANTAGE_API_KEY",
-    throw new RuntimeException("Clé API non définie dans les variables d'environnement"))
+  // Responses
+  sealed trait Response
+  case class StockPriceResponse(symbol: String, price: Option[Double]) extends Response
+  case class TopGainersResponse(gainers: List[String]) extends Response
+  case class RSIResponse(symbol: String, rsi: Option[Double]) extends Response
+  case class ErrorResponse(message: String) extends Response
 
-  val url = s"https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey=$apiKey"
+  def apply()(implicit system: ActorSystem[_]): Behavior[Command] = {
+    Behaviors.setup { context =>
+      implicit val ec: ExecutionContext = context.system.executionContext
 
-  // HTTP client to fetch stock data
-  def getStockData(): Future[String] = {
-    val responseFuture: Future[HttpResponse] = Http().singleRequest(HttpRequest(uri = url))
-    responseFuture.flatMap { response =>
-      Unmarshal(response.entity).to[String]
+      // Get API key from environment variables
+      val apiKey: String = sys.env.getOrElse("ALPHA_VANTAGE_API_KEY",
+        throw new RuntimeException("API key not defined in environment variables"))
+
+      Behaviors.receiveMessage {
+        case GetStockPrice(symbol, replyTo) =>
+          context.log.info(s"Fetching stock price for $symbol")
+          val url = s"https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=$symbol&interval=5min&apikey=$apiKey"
+
+          val responseFuture = Http().singleRequest(HttpRequest(uri = url))
+            .flatMap(response => Unmarshal(response.entity).to[String])
+            .map { data =>
+              implicit val formats: Formats = DefaultFormats
+              val json = parse(data)
+              val price = (json \ "Time Series (5min)").children.headOption.flatMap { item =>
+                (item \ "4. close").extractOpt[Double]
+              }
+              StockPriceResponse(symbol, price)
+            }
+
+          responseFuture.onComplete {
+            case Success(response) => replyTo ! response
+            case Failure(exception) =>
+              context.log.error(s"Error fetching stock price: ${exception.getMessage}")
+              replyTo ! ErrorResponse(exception.getMessage)
+          }
+
+          Behaviors.same
+
+        case GetTopGainers(replyTo) =>
+          context.log.info("Fetching top gainers")
+          val url = s"https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey=$apiKey"
+
+          val responseFuture = Http().singleRequest(HttpRequest(uri = url))
+            .flatMap(response => Unmarshal(response.entity).to[String])
+            .map { data =>
+              implicit val formats: Formats = DefaultFormats
+              val json = parse(data)
+              val gainers = (json \ "top_gainers").children.flatMap { item =>
+                (item \ "ticker").extractOpt[String]
+              }
+              TopGainersResponse(gainers)
+            }
+
+          responseFuture.onComplete {
+            case Success(response) => replyTo ! response
+            case Failure(exception) =>
+              context.log.error(s"Error fetching top gainers: ${exception.getMessage}")
+              replyTo ! ErrorResponse(exception.getMessage)
+          }
+
+          Behaviors.same
+
+        case GetRSI(symbol, replyTo) =>
+          context.log.info(s"Fetching RSI for $symbol")
+          val url = s"https://www.alphavantage.co/query?function=RSI&symbol=$symbol&interval=5min&time_period=14&series_type=close&apikey=$apiKey"
+
+          val responseFuture = Http().singleRequest(HttpRequest(uri = url))
+            .flatMap(response => Unmarshal(response.entity).to[String])
+            .map { data =>
+              implicit val formats: Formats = DefaultFormats
+              val json = parse(data)
+              val rsi = (json \ "Technical Analysis: RSI").children.headOption.flatMap { item =>
+                (item \ "RSI").extractOpt[Double]
+              }
+              RSIResponse(symbol, rsi)
+            }
+
+          responseFuture.onComplete {
+            case Success(response) => replyTo ! response
+            case Failure(exception) =>
+              context.log.error(s"Error fetching RSI: ${exception.getMessage}")
+              replyTo ! ErrorResponse(exception.getMessage)
+          }
+
+          Behaviors.same
+      }
     }
-  }
-
-  // Extract top gainers' tickers from the JSON response
-  def extractTopGainersTickers(data: String): List[String] = {
-    val json = parse(data)
-    (json \ "top_gainers").children.flatMap { item =>
-      (item \ "ticker").extractOpt[String]
-    }
-  }
-
-  // Actor to handle stock data retrieval
-  def stockDataActor: Behavior[String] = Behaviors.receiveMessage { case _ =>
-    val stockDataFuture = getStockData()
-
-    stockDataFuture.onComplete {
-      case Success(data) =>
-        val tickers = extractTopGainersTickers(data)
-        println("Top gainers tickers: " + tickers.mkString(", "))
-      case Failure(exception) =>
-        println("Erreur lors de la récupération des données : " + exception.getMessage)
-    }(executionContext)
-
-    Behaviors.same
   }
 }
